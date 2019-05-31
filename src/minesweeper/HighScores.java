20d9b62071596dd24eb0a0f4e1dced2c12fe27d7
elainemwang <32891543+elainemwang@users.noreply.github.com>
2019-05-31 09:21:28 -0700

    high scores dont work

--- src/minesweeper/HighScores.java
+++ src/minesweeper/HighScores.java
@@ -1,5 +1,8 @@
 package minesweeper;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,6 +13,69 @@ package minesweeper;
  *
  * @author elainewang
  */
-public class HighScores {
+public class HighScores extends Button implements Displayable{
+
+    public HighScores(){
+        setPos(500,20);
+        setHeight(20);
+        setWidth(70);
+    }
+    
+    @Override
+    public void pressed() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void draw(Graphics window) {
+        window.setColor(Color.BLACK);
+        window.drawRect(xPos, yPos, width, height);
+        window.drawString("Scores", xPos+20, yPos+15);
+    }
+    @Override
+    public void setPos(int x, int y) {
+        xPos = x;
+        yPos = y;
+    }
+
+    @Override
+    public void setX(int x) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setY(int y) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getX() {
+        return xPos;
+    }
+
+    @Override
+    public int getY() {
+        return yPos;
+    }
+
+    @Override
+    public int getWidth() {
+        return width;
+    }
+
+    @Override
+    public int getHeight() {
+        return height;
+    }
+
+    @Override
+    public void setWidth(int w) {
+        width = w;
+    }
+
+    @Override
+    public void setHeight(int h) {
+        height = h;
+    }
     
 }
